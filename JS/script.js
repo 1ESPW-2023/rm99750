124db11d48a7bcdfb4fca7e4773b80835d3ea255
FelipeCortez0 <rm99750@fiap.com.br>
2023-04-11 21:01:02 -0300

    AAAAA

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,12 @@
 //Exemplo funções
 const divElement = document.getElementById("div-box")
 
-divElement.addEventListener("click", ()=>{
+//divElement.addEventListener("click", ()=>{
+  //  console.log(divElement)
+//});
+
+divElement.addEventListener("click",mostraElemento );
+
+function mostraElemento () {
     console.log(divElement)
-});
+}
