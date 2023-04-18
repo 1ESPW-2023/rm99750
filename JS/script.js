a4c881ea49ab5a73b85e7744cd809951f790b86f
FelipeCortez0 <rm99750@fiap.com.br>
2023-04-18 20:17:47 -0300

    Ferrou

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,38 @@
-//Exemplo funções
-const divElement = document.getElementById("div-box")
+// //Exemplo funções
+// const divElement = document.getElementById("div-box")
 
-//divElement.addEventListener("click", ()=>{
-  //  console.log(divElement)
-//});
+// //divElement.addEventListener("click", ()=>{
+//   //  console.log(divElement)
+// //});
 
-divElement.addEventListener("click",mostraElemento );
+// divElement.addEventListener("click",mostraElemento );
 
-function mostraElemento () {
-    console.log(divElement)
+
+// function mostraElemento () {
+//     console.log(divElement)
+// }
+
+// function soma() {
+//   let n1 = 10
+//   let n2 = 5
+//   console.log(n1 +n2)
+// }
+
+// //Recuperando elementos do HTML com querySelectorAll
+// const liElements = document.querySelectorAll("li");
+
+// //Convertendo node list em array com spread
+// const arrayElements = [...liElements];
+// console.log(liElements);
+
+//Convertendo node list em array com spread
+const arrayElements = [...document.querySelectorAll("li")];
+
+arrayElements.map((el,key)=>{
+
+  console.log(el.textContent.indexOf)
+
+  if (el.textContent.indexOf("item")) {
+      el.textContent = `[${el.textcontent}]`;
 }
+});
